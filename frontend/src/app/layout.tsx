1f7407769b0d73485b473ac0cfdb39037bc68e95
rubenaprikyan <rubenaprikyan@gmail.com>
2024-02-06 05:38:56 +0400

    [setup] Add next project

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        Welcome to Next app
+        {children}
+      </body>
+    </html>
+  );
+}
